e168092705d72609acac221b745eacfca2c5377f
Fellipe Salazar <fellipesalazar97@gmail.com>
2021-10-04 21:56:23 -0300

    add plugin jquery ui

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,45 @@
-$("section").click(function(){
-    $(this).toggleClass("active");
+// call all functions
+$(function(){
+    enableSortableprojects();
+    openDropdownMenu(".user-info span",".user-info .dropdown-menu",".user-info *");
+    openDropdownMenu(".notifications ion-icon",".notifications .dropdown-menu",".notifications *");
+    // enableActiveScroll();
 });
+
+
+// sortable effect
+function enableSortableprojects(){
+    $(".project-list").sortable({
+        connectWith: ".project-list",
+        placeholder: "ui-state-highlight",
+        cursor: "grabbing",
+        classes: {
+            "ui-sortable-helper": "grab"
+        },
+        dropOnEmpty: true
+    }).disableSelection();
+}
+
+// open dropdow menu
+function openDropdownMenu(toggle, menu, target){
+    $(toggle).click(function(){
+        $(menu).fadeToggle(250);
+    });
+    $("body").click(function(e){
+        var condition = $(e.target).is($(target));
+        if(!condition){
+            $(menu).fadeOut(250);
+        }
+    });
+}
+
+// // scroll effects
+// function enableActiveScroll(){
+//     $(window).scroll(function(){
+//         if($(this).scrollTop() > 10){
+//             $("header,.content").addClass("active");
+//             return;
+//         }
+//         $("header,.content").removeClass("active");
+//     });
+// }
